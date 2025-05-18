d02aa2f284cf119366230cbd6f516bd62c5599c8
Vishnu S <vishnus082005@gmail.com>
2025-05-19 00:26:07 +0530

    Integrated Donate Card Successfully

--- CrowdFunding/Client/src/CampaignDetailPage/CampaignDetail.jsx
+++ CrowdFunding/Client/src/CampaignDetailPage/CampaignDetail.jsx
@@ -1,10 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import donateDirection from "../../../Images/donateDirection.png";
 
 function CampaignDetail() {
   const [searchParam] = useSearchParams();
+  let [warning, setWarning] = useState(null);
+  let [donateCard, setDonateCard] = useState(false);
+
+  const showDonateCard = () => {
+    setDonateCard(!donateCard);
+  };
+
   return (
-    <div className="flex justify-center items-center h-[100vh] w-[100vw] bg-gray-100 pt-[100px]">
+    <div className="flex justify-center items-center h-[100vh] w-[100vw] bg-gray-100 pt-[100px] relative">
+      {donateCard == false ? (
+        <div className="h-[100vh] w-[100vw] flex items-center justify-center fixed z-[5] bg-white bg-opacity-75 top-[20px]">
+          <div className="h-[590px] w-[400px] border-2 border-red-800 rounded-[10px] flex flex-col justify-between items-center px-[15px] py-[20px] bg-blue-950 relative">
+            <div
+              className="absolute top-[-24px] right-[-20px] h-[25px] w-[25px] rounded-[50%] bg-red-700 text-white text-[20px] flex items-center justify-center hover:bg-red-500 cursor-pointer"
+              onClick={showDonateCard}
+            >
+              x
+            </div>
+            <div className="flex items-center justify-center text-[30px] font-bold text-white">
+              Donate
+            </div>
+            <div className="flex items-center justify-between w-[100%] relative ">
+              <img
+                src={donateDirection}
+                className="absolute h-[60px] w-[60px] top-[10px] right-[160px]"
+              />
+              <div className="flex flex-col w-[50%] items-center justify-between">
+                <img
+                  className="border-2 border-white h-[80px] w-[80px] rounded-[50%] object-cover mb-[5px]"
+                  src="https://imgs.search.brave.com/bhkFZKyzmusTKy4NX1rt7Cu5yOaVovZUfFTiY5PA5Ts/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLmRh/d24uY29tL2xhcmdl/LzIwMjUvMDUvMDUx/NDE5NTc4OTVmMmQ4/LmpwZz9yPTE0MzIz/NA"
+                />
+                <p className="text-white">Trisha</p>
+              </div>
+              <div className="flex flex-col w-[50%] items-center justify-between">
+                <img
+                  className="border-2 border-white h-[80px] w-[80px] rounded-[50%] object-cover mb-[5px]"
+                  src="https://imgs.search.brave.com/bhkFZKyzmusTKy4NX1rt7Cu5yOaVovZUfFTiY5PA5Ts/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLmRh/d24uY29tL2xhcmdl/LzIwMjUvMDUvMDUx/NDE5NTc4OTVmMmQ4/LmpwZz9yPTE0MzIz/NA"
+                />
+                <p className="text-white">Ane De Armas</p>
+              </div>
+            </div>
+            <div className="flex items-center justify-between w-[100%]">
+              <div className="text-white">Account Balance</div>
+              <div className="text-white">$55555</div>
+            </div>
+            <div className="flex items-center justify-between w-[100%]">
+              <div className="text-white">Minimum Donation Amount</div>
+              <div className="text-white">$1000</div>
+            </div>
+            <div className="flex items-center justify-between w-[100%]">
+              <div className="text-white">Transaction Fee</div>
+              <div className="text-white">$5</div>
+            </div>
+            <input
+              placeholder="Enter Donation Amount (RUPEES)"
+              type="number"
+              className="h-[40px] w-[100%] rounded-[5px]  border-2  border-white border-opacity-20 bg-blue-950 p-[15px] text-white text-[20px] focus:outline-none focus:border-white"
+            />
+            <div className="text-yellow-500 w-[100%]">
+              {warning == null
+                ? `Note : Maximum Donatable Amount => $${500} `
+                : "that"}
+            </div>
+            <button className="w-[100%] h-[45px] bg-yellow-500 text-[20px] font-extrabold rounded-[10px] hover:bg-yellow-400">
+              Donate Now
+            </button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
       <div className="flex flex-col  h-[543px] w-[50vw] rounded-[15px] overflow-hidden bg-white">
         <div className="relative h-[55%] w-full text-white">
           <img
@@ -52,7 +122,10 @@ function CampaignDetail() {
 
           <div className="flex flex-col items-center justify-between h-full w-[30%]  px-[10px] py-[14px] bg-gray-100 rounded-[10px]">
             <h1 className="text-[20px] font-semibold">Make a Donation</h1>
-            <div className="flex flex-col items-center justify-center h-[115px] w-[115px] rounded-[50%] bg-red-700 bg-orange-600 text-[24px] font-semibold text-white italic hover:scale-110 duration-200 transition-all ease-in-out transform hover:bg-orange-500 cursor-pointer">
+            <div
+              className="flex flex-col items-center justify-center h-[115px] w-[115px] rounded-[50%] bg-red-700 bg-orange-600 text-[24px] font-semibold text-white italic hover:scale-110 duration-200 transition-all ease-in-out transform hover:bg-orange-500 cursor-pointer"
+              onClick={showDonateCard}
+            >
               <h1>Donate</h1>
               <h1>Now</h1>
             </div>
